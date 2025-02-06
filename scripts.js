9a475e8b0d806b5125e5c1830cfe5ab64eca8189
Mr-McSizzle <76867623+Mr-McSizzle@users.noreply.github.com>
2025-02-07 00:52:40 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('yes-button').addEventListener('click', function() {
-    document.getElementById('message').textContent = "Yay! I'm so happy! 💖";
-    document.getElementById('yes-button').disabled = true;
-    document.getElementById('no-button').disabled = true;
-});
-
-document.getElementById('no-button').addEventListener('click', function() {
-    document.getElementById('no-button').style.display = 'none';
-});
