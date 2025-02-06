bc7ffbe4b6c686924d9194adcc53571e580e06c2
Mr-McSizzle <76867623+Mr-McSizzle@users.noreply.github.com>
2025-02-07 00:39:00 +0530

    Delete files directory

--- files/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('yes-button').addEventListener('click', function() {
-    alert('Yay! I\'m so happy!');
-});
-
-document.getElementById('no-button').addEventListener('click', function() {
-    alert('Oh no! Maybe next time.');
-});
