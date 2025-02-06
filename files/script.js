599e46ef8de513370d4c3c9e2d0e7c838757dfc6
Mr-McSizzle <76867623+Mr-McSizzle@users.noreply.github.com>
2025-02-07 00:28:47 +0530

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,7 @@
+document.getElementById('yes-button').addEventListener('click', function() {
+    alert('Yay! I\'m so happy!');
+});
+
+document.getElementById('no-button').addEventListener('click', function() {
+    alert('Oh no! Maybe next time.');
+});
